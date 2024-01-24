8201357e97b70261cae99d471fe3aa41b55a1679
dona <>
2024-01-24 14:03:56 +0100

    second project Types

---
+++ index.ts
@@ -0,0 +1,54 @@
+import products from './products';
+
+const productName: string = 'fanny pack';
+let shipping: number;
+let taxPercent: number;
+let taxTotal: number;
+let total: number;
+const shippingAddress: string = 'Wilhelmstraße 40, 69221 Dossenheim';
+
+const product = products.find((product) => product.name === productName);
+
+if (product) {
+  console.log('Found product:', product);
+} else {
+  console.log('Product not found');
+}
+
+if (product !== undefined && product.preOrder === true) {
+  console.log('We will send you a message when the product is on the way!');
+} else {
+  console.log('Product is not on pre-order.');
+}
+
+if (product !== undefined && product.price > 25) {
+  shipping = 0;
+  console.log('Free shipping for this product!')
+} else {
+  shipping = 5;
+  console.log('Standard shipping fee applied.')
+}
+
+if (shippingAddress.match('New York')) {
+  taxPercent = 0.1;
+} else {
+  taxPercent = 0.05;
+}
+//Alternative:
+/* if (shippingAddress.toLowerCase().includes('New York')) {
+    taxPercent = 0.1; // 10% tax for New York
+  } else {
+    taxPercent = 0.05; // 5% tax for other locations
+  } */
+
+taxTotal = product.price * taxPercent;
+total = product.price + taxTotal + shipping;
+
+console.log(`
+Product:  ${product.name}
+Address:  ${shippingAddress}
+Price:    $${product.price}
+Tax:      $${taxTotal.toFixed(2)}
+Shipping: $${shipping.toFixed(2)}
+Total:    $${total.toFixed(2)}
+`);
